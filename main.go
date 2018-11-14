b2d47d18f7a388690efb0dbf2e5b62539e836dfc
Chase Weaver <chase.weaver34@gmail.com>
2018-11-14 11:57:10 -0500

    Testing

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/perlin-network/noise/crypto/ed25519"
+	"github.com/perlin-network/noise/examples/chat/messages"
+	"github.com/perlin-network/noise/network"
+	"github.com/perlin-network/noise/network/discovery"
+	"github.com/perlin-network/noise/types/opcode"
+)
+
+type chatPlugin struct{ *network.Plugin }
+
+func (state *chatPlugin) Receive(ctx *network.PluginContext) error {
+	switch msg := ctx.Message().(type) {
+	case *messages.ChatMessage:
+		log.Println(fmt.Printf("<%s> %s", ctx.Client().ID.Address, msg.Message))
+	}
+	return nil
+}
+
+func main() {
+	// process other flags
+	portFlag := flag.Int("port", 3000, "port to listen to")
+	hostFlag := flag.String("host", "localhost", "host to listen to")
+	protocolFlag := flag.String("protocol", "tcp", "protocol to use (kcp/tcp)")
+	peersFlag := flag.String("peers", "", "peers to connect to")
+	flag.Parse()
+
+	port := uint16(*portFlag)
+	host := *hostFlag
+	protocol := *protocolFlag
+	peers := strings.Split(*peersFlag, ",")
+
+	keys := ed25519.RandomKeyPair()
+
+	log.Println(fmt.Printf("Private Key: %s", keys.PrivateKeyHex()))
+	log.Println(fmt.Printf("Public Key: %s", keys.PublicKeyHex()))
+
+	opcode.RegisterMessageType(opcode.Opcode(1000), &messages.ChatMessage{})
+	builder := network.NewBuilder()
+	builder.SetKeys(keys)
+	builder.SetAddress(network.FormatAddress(protocol, host, port))
+
+	// Register peer discovery plugin.
+	builder.AddPlugin(new(discovery.Plugin))
+
+	// Add custom chat plugin.
+	builder.AddPlugin(new(chatPlugin))
+
+	net, err := builder.Build()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	go net.Listen()
+
+	if len(peers) > 0 {
+		net.Bootstrap(peers...)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+
+		// skip blank lines
+		if len(strings.TrimSpace(input)) == 0 {
+			continue
+		}
+
+		log.Println(fmt.Printf("<%s> %s", net.Address, input))
+
+		ctx := network.WithSignMessage(context.Background(), true)
+		net.Broadcast(ctx, &messages.ChatMessage{Message: input})
+
+		log.Println(net.GetKeys().PrivateKey)
+
+		log.Println(net.GetKeys().PublicKey)
+	}
+}
