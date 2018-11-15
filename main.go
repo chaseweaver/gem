8ee9a3eef528b067d2c125fb48cc720fd0ba7ec1
Chase Weaver <chase.weaver34@gmail.com>
2018-11-15 01:54:13 -0500

    Testing

--- main.go
+++ main.go
@@ -1,88 +1,64 @@
 package main
 
 import (
-	"bufio"
-	"context"
 	"flag"
-	"fmt"
-	"log"
-	"os"
-	"strings"
 
-	"github.com/perlin-network/noise/crypto/ed25519"
-	"github.com/perlin-network/noise/examples/chat/messages"
-	"github.com/perlin-network/noise/network"
-	"github.com/perlin-network/noise/network/discovery"
-	"github.com/perlin-network/noise/types/opcode"
+	astilectron "github.com/asticode/go-astilectron"
+	bootstrap "github.com/asticode/go-astilectron-bootstrap"
+	astilog "github.com/asticode/go-astilog"
+	"github.com/pkg/errors"
+	"github.com/thibran/pubip"
 )
 
-type chatPlugin struct{ *network.Plugin }
+var (
+	w       *astilectron.Window
+	l       listener
+	appName string
+	builtAt string
+)
 
-func (state *chatPlugin) Receive(ctx *network.PluginContext) error {
-	switch msg := ctx.Message().(type) {
-	case *messages.ChatMessage:
-		log.Println(fmt.Printf("<%s> %s", ctx.Client().ID.Address, msg.Message))
-	}
-	return nil
-}
-
-func port() {
-	// process other flags
-	portFlag := flag.Int("port", 3000, "port to listen to")
-	hostFlag := flag.String("host", "localhost", "host to listen to")
-	protocolFlag := flag.String("protocol", "tcp", "protocol to use (kcp/tcp)")
-	peersFlag := flag.String("peers", "", "peers to connect to")
+func main() {
 	flag.Parse()
+	astilog.FlagInit()
+	astilog.Debugf("Running app built at %s", builtAt)
+	if err := bootstrap.Run(bootstrap.Options{
+		Asset:    Asset,
+		AssetDir: AssetDir,
+		AstilectronOptions: astilectron.Options{
+			AppName: appName,
+		},
+		OnWait: func(_ *astilectron.Astilectron, ws []*astilectron.Window, _ *astilectron.Menu, _ *astilectron.Tray, _ *astilectron.Menu) error {
+			w = ws[0]
+			ip, _ := pubip.NewMaster().Address()
+			go func() {
+				if err := bootstrap.SendMessage(w, "ip", ip); err != nil {
+					return
+				}
+			}()
 
-	port := uint16(*portFlag)
-	host := *hostFlag
-	protocol := *protocolFlag
-	peers := strings.Split(*peersFlag, ",")
-
-	keys := ed25519.RandomKeyPair()
-
-	log.Println(fmt.Printf("Private Key: %s", keys.PrivateKeyHex()))
-	log.Println(fmt.Printf("Public Key: %s", keys.PublicKeyHex()))
-
-	opcode.RegisterMessageType(opcode.Opcode(1000), &messages.ChatMessage{})
-	builder := network.NewBuilder()
-	builder.SetKeys(keys)
-	builder.SetAddress(network.FormatAddress(protocol, host, port))
-
-	// Register peer discovery plugin.
-	builder.AddPlugin(new(discovery.Plugin))
-
-	// Add custom chat plugin.
-	builder.AddPlugin(new(chatPlugin))
-
-	net, err := builder.Build()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	go net.Listen()
-
-	if len(peers) > 0 {
-		net.Bootstrap(peers...)
-	}
-
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		input, _ := reader.ReadString('\n')
-
-		// skip blank lines
-		if len(strings.TrimSpace(input)) == 0 {
-			continue
-		}
-
-		log.Println(fmt.Printf("<%s> %s", net.Address, input))
-
-		ctx := network.WithSignMessage(context.Background(), true)
-		net.Broadcast(ctx, &messages.ChatMessage{Message: input})
-
-		log.Println(net.GetKeys().PrivateKey)
-
-		log.Println(net.GetKeys().PublicKey)
+			// Sets host as outward IP of port 3000 as TCP
+			l.SetIP(ip)
+			l.SetPort("3000")
+			l.SetProtocol("tcp")
+			return nil
+		},
+		RestoreAssets: RestoreAssets,
+		Windows: []*bootstrap.Window{{
+			Homepage:       "index.html",
+			MessageHandler: messageHandler,
+			Options: &astilectron.WindowOptions{
+				Title:           astilectron.PtrStr("GEM : Go Encryption Messenger"),
+				BackgroundColor: astilectron.PtrStr("#f3f3f6"),
+				Frame:           astilectron.PtrBool(false),
+				Resizable:       astilectron.PtrBool(false),
+				HasShadow:       astilectron.PtrBool(false),
+				Fullscreenable:  astilectron.PtrBool(false),
+				Center:          astilectron.PtrBool(true),
+				Height:          astilectron.PtrInt(480),
+				Width:           astilectron.PtrInt(650),
+			},
+		}},
+	}); err != nil {
+		astilog.Fatal(errors.Wrap(err, "running bootstrap failed"))
 	}
 }
